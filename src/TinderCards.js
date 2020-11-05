ddd9ad96792dd4d80f8e9ca336fad9bfc85b0ac6
shubham_patil <shubham.patil@cdk.com>
2020-11-05 11:09:53 +0530

    Hooking up our Front-End and Back-End

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,23 +1,19 @@
-import React, { useState }from 'react'
+import React, { useState, useEffect }from 'react'
 import "./TinderCards.css"
 import TinderCard from 'react-tinder-card'
 import { SwipeableDrawer } from '@material-ui/core';
-
-
-
+import axios from './axios';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-         {
-             name: 'Elon Musk',
-             url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg"
-         },
-         {
-            name: 'Jeff Bezos',
-            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D560%26cropX2%3D1783%26cropY1%3D231%26cropY2%3D1455"
-        }   
+    const [people, setPeople] = useState([]);
 
-    ]);
+    useEffect(() => {
+       async function fetchData() {
+           const req = await axios.get('/tinder/cards'); 
+           setPeople(req.data);
+       }
+       fetchData();
+    }, []);
 
     const Swiped = (direction, nameToDelete) => {
          console.log('removing : '+ nameToDelete);
@@ -40,7 +36,7 @@ function TinderCards() {
                         onSwipe={(dir) => Swiped(dir, person.name)}
                         onCardLeftScreen={()=> outOfFrame(person.name)}
                         >
-                            <div style={{backgroundImage: `url(${person.url})`}}
+                            <div style={{backgroundImage: `url(${person.imgUrl})`}}
                             className="card">
                                 <h3>{person.name}</h3>
                             </div>
